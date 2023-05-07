7b4ecb56e66331c9b2113c80d52be89e02085a5c
woodstock <108785772+Stilllee@users.noreply.github.com>
2023-05-08 00:15:14 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,10 +1,5 @@
 function solution(num_list) {
-    let answer = 0;
-    let odd = "";
-    let even = "";
-    for(let i = 0; i < num_list.length; i++){
-        (num_list[i] % 2 !== 0) ? odd += num_list[i] : even += num_list[i];
-        answer = Number(odd) + Number(even);
-    }
-    return answer;
+    const odd = num_list.filter(n => n % 2 !== 0).join('');
+    const even = num_list.filter(n => n % 2 === 0).join('');
+    return Number(odd) + Number(even);
 }
