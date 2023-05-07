1d2cbb81d83e3ba2573b4f1bd83992d2b517179a
woodstock <108785772+Stilllee@users.noreply.github.com>
2023-05-07 22:10:01 +0900

    [unrated] Title: 코드 처리하기, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,23 @@
+function solution(code) {
+    let ret = "";
+    let mode = 0;
+    
+    for(idx=0; idx<code.length; idx++){
+        if(mode){
+            if(code[idx] !== "1" && idx % 2 !== 0){
+                ret += code[idx];
+            } else if (code[idx] === "1"){
+                mode = 0;
+            }            
+        } else {
+            if(code[idx] !== "1" && idx % 2 === 0){
+                ret += code[idx];
+            } else if (code[idx] === "1"){
+                mode = 1;
+            }            
+        }
+    }
+    return ret === "" ? "EMPTY" : ret;
+}
+    
+    
