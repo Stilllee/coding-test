071d9816daa039fc78c88f489ac0512a8bee109a
woodstock <108785772+Stilllee@users.noreply.github.com>
2023-09-29 17:13:21 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,13 +1,6 @@
-function solution(n) {
-    let answer = [];
-    while(n !== 1){
-        answer.push(n);
-        if(n % 2 === 0){
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
-        }
-    }
-    answer.push(1);
-    return answer;
+function solution(n, arr = []){
+  arr.push(n); // 현재 숫자 n을 배열에 넣는다.
+  if (n === 1) return arr; // n이 1이면, 더 이상 변환할 필요가 없으니 배열을 반환한다.
+  if (n % 2 === 0) return solution(n / 2, arr); // n이 짝수면, 2로 나눈 값으로 함수를 다시 호출 (재귀 호출)
+  return solution(3 * n + 1, arr); // n이 홀수면, 3을 곱하고 1을 더한 값으로 함수를 다시 호출
 }
