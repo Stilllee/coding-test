5776cc6cce32a607a93b19ad2e8870ef29eceba6
woodstock <108785772+Stilllee@users.noreply.github.com>
2023-11-25 03:42:22 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,5 +1,8 @@
 function solution(a, b) {
-    if (a % 2 !== b % 2) return 2 * (a + b);
-    if (a % 2 === 0 && b % 2 === 0) return Math.abs(a - b);
-    return Math.pow(a, 2) + Math.pow(b, 2);
+    const isAOdd = a % 2 === 1;
+    const isBOdd = b % 2 === 1;
+    
+    if(isAOdd && isBOdd) return Math.pow(a, 2) + Math.pow(b, 2);
+    if(!isAOdd && !isBOdd) return Math.abs(a - b);  
+    return 2 * (a + b);
 }
