27671917d9fd0ec06cdbf554d6ee7393fde25348
woodstock <108785772+Stilllee@users.noreply.github.com>
2023-12-04 13:44:50 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 3.51 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -0,0 +1,9 @@
+function solution(arr, k) {
+    return arr.map((item) => {
+        if(k % 2 === 0) {
+            return item + k;
+        } else {
+            return item * k;
+        }
+    })
+}
