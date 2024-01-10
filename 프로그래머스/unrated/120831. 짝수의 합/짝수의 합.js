3fda1d668389528229bed41b82ad47a526b1bd42
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-01-11 00:12:41 +0900

    [level unrated] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,4 @@
 const solution = n => {
-    let result = 0;
-    for(let i = 2; i <= n; i+=2) {
-        result += i;
-    }
-    return result;
-}
+  const count = Math.floor(n / 2);
+  return count * (2 + (count * 2)) / 2;
+};
