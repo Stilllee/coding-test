0486db76ddfd6b2fb087d11c795d3a5913c7d67d
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-01-21 21:04:38 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,11 +1,7 @@
-const solution = num_list => {
-    const result = [];
-    let odd = 0;
-    let even = 0;
-    for (let i = 0; i < num_list.length; i++) {
-        num_list[i] % 2 === 0 ? even++ : odd++;
+function solution(num_list) {
+    const result = [0, 0];
+    for (let a of num_list) {
+        result[a % 2] += 1;
     }
-    result.push(even);
-    result.push(odd);
     return result;
 }
