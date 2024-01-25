04b666f467ffc6413250ad274fbe3c7ef7820e2b
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-01-25 16:38:44 +0900

    [level 0] Title: 세로 읽기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,10 +1,7 @@
 function solution (str, m, c) {
-    if (m === 1) return str;
     let result = '';
-    for(let i = 0; i < str.length; i+= m) {
-        if (i + c - 1 < str.length) {
-            result += str[i + c - 1];
-        }        
+    for(let i = c - 1; i < str.length; i+= m) {
+            result += str[i];
     }
     return result;
 }
