bff090a6d9bd108d53ba0db1a64ee6d56325193f
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-02-22 00:44:40 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,18 @@
+function solution(n, slicer, num_list) {
+    const [a, b, c] = slicer;
+    
+    function cutter(x, y){
+        return num_list.slice(x, y);
+    }
+
+    switch(n){
+        case n = 1:
+            return cutter(0, b + 1);    
+        case n = 2:
+            return cutter(a);
+        case n = 3:
+            return cutter(a, b + 1);
+        default:
+            return cutter(a, b + 1).filter((v, i) => i % c === 0);
+    }
+}
