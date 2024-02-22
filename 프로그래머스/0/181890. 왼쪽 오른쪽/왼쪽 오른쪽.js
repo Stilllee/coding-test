00ff052d862fb9b56aa839153aa56877a3f20aed
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-02-22 16:10:17 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,10 +1,7 @@
-function solution(list){
-  const includeL = list.indexOf('l');
-  const includeR = list.indexOf('r');
-
-  if (includeL !== -1 && (includeR === -1 || includeL < includeR)) return list.slice(0, includeL);
-  
-  if (includeR !== -1 && (includeL === -1 || includeR < includeL)) return list.slice(includeR + 1);
-  
-  return [];
+function solution(list) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === 'l') return list.slice(0, i);
+        if (list[i] === 'r') return list.slice(i + 1);
+    }
+    return [];
 }
