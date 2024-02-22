e1fa6fcecaa1962fece058522b26f1112eb5d2e5
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-02-22 13:31:44 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.50 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,4 +1,4 @@
 function solution(arr, intervals) {
-    const [a1, b1, a2, b2] = intervals.flat();
-    return arr.slice(a1, b1 + 1).concat(arr.slice(a2, b2 + 1));
+    let [[a, b], [c, d]] = intervals;
+    return [...arr.slice(a, b + 1), ...arr.slice(c, d + 1)];
 }
