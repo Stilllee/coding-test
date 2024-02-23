43ee6a992ebef42194f0bbb8bf87f1d126141ba6
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-02-23 16:35:45 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.57 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,10 +1,6 @@
-function solution(arr, queries) {    
-    for (let i = 0; i < queries.length; i++) {
-        const start = queries[i][0];
-        const end = queries[i][1];
-        for (let j = start; j <= end; j++) {
-            arr[j] += 1;
-        }
-    }
+function solution(arr, queries) {
+    queries.forEach(([s, e]) => {
+        while (s <= e) arr[s++]++;
+    });
     return arr;
 }
