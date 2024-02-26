9d5cb78f94ee41119bafecdfa75e468c58cf5664
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-02-26 11:36:34 +0900

    [level 0] Title: 1로 만들기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,16 @@
+function solution(list) {
+    let count = 0;
+
+    for (let num of list) {
+        while (num > 1) {
+            if (num % 2 === 0) {
+                num /= 2;
+            } else {
+                num = (num - 1) / 2;
+            }
+            count++;
+        }
+    }
+    
+    return count;
+}
