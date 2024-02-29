419d04dcccfe20da3d5e04aa7538496014e20b8f
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-02-29 14:07:06 +0900

    [level 0] Title: ad 제거하기, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -1,9 +1,1 @@
-function solution(strArr) {
-    var answer = [];
-    for (let i = 0; i < strArr.length; i++) {
-        if (!strArr[i].includes('ad')) {
-            answer.push(strArr[i]);
-        }
-    }
-    return answer;
-}
+const solution = (strArr) => strArr.filter(v => !v.includes('ad'));
