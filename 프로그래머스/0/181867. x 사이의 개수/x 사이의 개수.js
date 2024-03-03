cadd2a9e0871b386aa7183777c5033df74c8b846
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-03-03 13:54:42 +0900

    [level 0] Title: x 사이의 개수, Time: 1.30 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.js
@@ -1,8 +1,1 @@
-function solution(myString) {
-    let result = [];
-    const arr = myString.split('x');
-    for (let i = 0; i < arr.length; i++) {
-        result.push(arr[i].length);
-    }
-    return result;
-}
+const solution = (myString) => myString.split('x').map(v => v.length);
