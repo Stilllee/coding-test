0ac2223647ef539f75c87bcac282d71c43de70fb
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-04-07 17:19:01 +0900

    [level 0] Title: 커피 심부름, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,12 @@
+function solution(order) {
+    let count = 0;
+    
+    for (let i = 0; i < order.length; i++) {
+        if(order[i].includes('latte')) {
+            count += 5000;
+        } else {
+            count += 4500;
+        }
+    }
+    return count;
+}
