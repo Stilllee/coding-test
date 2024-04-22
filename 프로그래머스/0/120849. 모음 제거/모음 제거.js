cc8950cdf2085be885933d649d173ca87aa62547
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-04-23 00:09:48 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,11 +1,3 @@
 function solution(my_string) {
-    var answer = '';
-    const vowel = ['a', 'e', 'i', 'o', 'u'];
-    
-    for(let i = 0; i < my_string.length; i++) {
-        if(!vowel.includes(my_string[i])) {
-            answer += my_string[i];
-        }
-    }    
-    return answer;
+    return Array.from(my_string).filter(v => !['a', 'e', 'i', 'o', 'u'].includes(v)).join('');
 }
