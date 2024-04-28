c007a806578d7e59bed9d021dda18f2e48adc411
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-04-28 22:52:05 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,7 +1,10 @@
 function solution(n) {
-    let pizza = 1;
-    while (pizza * 6 % n) {
-        pizza++;
+    let piece = 6;
+    
+    while (true) {
+        if (piece % n === 0) break;
+        piece += 6;
     }
-    return pizza;
+    
+    return piece / 6;
 }
