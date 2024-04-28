4e3ab512bb50c41460ec860c117366d136bbbbaa
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-04-28 22:47:19 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,10 +1,5 @@
-// 피자 균등 분배 방법
-// (n과 6의 최소공배수) / 6 === n / (n과 6의 최대공약수)
-
-// 최대공약수를 구하는 함수
-const gcd = (a, b) => b == 0 ? a : gcd(b, a % b);
-
 function solution(n) {
-    let g = gcd(6, n);
-    return (n / g);
+    for (let i = n; ; i+=n) {
+        if (i % 6 === 0) return i / 6;
+    }
 }
