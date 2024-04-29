45c5486beaa253d7b8822b5d64c521697390d85f
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-04-29 12:27:29 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,3 +1,9 @@
+// 문자열 자체도 인덱스 접근 가능
+
 function solution(age) {
-    return age.toString().split('').map(c => String.fromCharCode(97 + +c)).join('');
+    return age
+    .toString()
+    .split('')
+    .map((c) => 'abcdefghij'[c])
+    .join('');
 }
