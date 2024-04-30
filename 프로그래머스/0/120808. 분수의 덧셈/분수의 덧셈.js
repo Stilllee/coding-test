ba7e68cb29709e5667a80716280f7817487c3089
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-04-30 11:17:14 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,12 +1,9 @@
-function gcd(a, b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
-}
+const gcd = (a, b) => b == 0 ? a : gcd(b, a % b);
 
 function solution(n1, d1, n2, d2) {
-    const n = (n1 * d2) + (n2 * d1);
-    const d = d1 * d2;    
-    const g = gcd(n, d);    
+    const n = (d1 * n2) + (d2 * n1);
+    const d = d1 * d2;
+    const g = gcd(n, d);
     
-    return [n / g, d / g];
+    return [n/g, d/g];
 }
