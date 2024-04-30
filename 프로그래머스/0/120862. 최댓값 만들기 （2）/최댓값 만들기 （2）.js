6510b19648ca9164ceb609f980cfa56eee72a397
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-04-30 10:43:33 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,7 +1,8 @@
 function solution(numbers) {
     const sortedNum = [...numbers].sort((a, b) => b - a);
-    const max1 = sortedNum[0] * sortedNum[1];
-    const max2 = sortedNum[sortedNum.length - 1] * sortedNum[sortedNum.length - 2];
+    
+    const max1 = sortedNum[0] * sortedNum[1];   // 원소 두 개가 모두 양수일 경우
+    const max2 = sortedNum[sortedNum.length - 1] * sortedNum[sortedNum.length - 2]; // 원소 두 개가 모두 음수일 경우
     
     return Math.max(max1, max2);
 }
