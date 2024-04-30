30e36f4eb27097a625cc9f1b64977a863747c8ab
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-04-30 10:21:10 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,4 +1,3 @@
 function solution(box, n) {
-    const [width, depth, height] = box;
-    return Math.floor(width / n) * Math.floor(depth / n) * Math.floor(height / n);
+    return box.reduce((acc, v) => acc * Math.floor(v / n), 1);
 }
