f33bc97fa0bc068e95454f66119c377fbab86ff8
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-05-08 10:37:33 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,10 +1,5 @@
 function solution(str, num1, num2) {
     const strArr = [...str];
-    const a = strArr[num1];
-    const b = strArr[num2];
-    
-    strArr[num1] = b;
-    strArr[num2] = a;
-    
+    [strArr[num1], strArr[num2]] = [strArr[num2], strArr[num1]];
     return strArr.join('');
 }
