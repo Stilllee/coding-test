a7e23aa238b12eb13473c20da74e700ed2343606
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-05-09 13:15:21 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,5 +1,5 @@
-function solution(n) {
-    for (i = 2; i < n; i++ ) {
-        if (n % i === 1) return i;
+function solution(n, x = 1) {
+    while (x++) {
+        if ( n % x === 1) return x;
     }
 }
