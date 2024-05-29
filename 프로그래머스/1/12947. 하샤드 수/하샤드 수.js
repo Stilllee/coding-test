0612eaf4b51900ccdb6abda2ded04859a5a23667
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-05-29 15:35:51 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -3,5 +3,5 @@ function solution(x) {
     let arr = str.split('').map(Number);
     let sum = arr.reduce((acc, cur) => acc += cur);
     
-    return x % sum === 0 ? true : false;
+    return x % sum === 0;
 }
