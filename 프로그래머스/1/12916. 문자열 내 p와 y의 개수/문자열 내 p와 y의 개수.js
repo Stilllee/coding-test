20b482fec8e31a811319f2748266acd7463e389a
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-05-29 15:17:40 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,8 +1,16 @@
 function solution(s){
     let lowerCaseStr = s.toLowerCase();
     
-    let pCount = (lowerCaseStr.match(/p/g) || []).length;
-    let yCount = (lowerCaseStr.match(/y/g) || []).length;
+    let pCount = 0;
+    let yCount = 0;
+    
+    for (let char of lowerCaseStr) {
+        if (char === 'p') {
+            pCount++;
+        } else if (char === 'y') {
+            yCount++;
+        }
+    }
 
     return pCount === yCount;
 }
