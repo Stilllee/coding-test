aaf5807a16930918367eefc42d97c91ba6fe271f
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-05-29 15:24:36 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -2,5 +2,7 @@ function solution(n) {
     let str = n.toString();
     let arr = str.split('').reverse();
     
-    return arr.map(item => Number(item));
+    // arr.map(Number)는 각 배열 요소를 Number 함수에 전달하여 숫자로 변환
+    // 이는 arr.map(item => Number(item))와 동일하게 작동
+    return arr.map(Number);
 }
