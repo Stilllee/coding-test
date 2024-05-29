ab654319ff71d9ebbe382d3489e2d9d4c80624da
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-05-29 15:50:42 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.27 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    // n x n 크기의 이차원 배열 초기화
+    var answer = Array.from({length: n}, () => Array(n).fill(0));
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === j) {
+                answer[i][j] = 1;
+            }
+        }
+    }
+    
+    return answer;
+}
