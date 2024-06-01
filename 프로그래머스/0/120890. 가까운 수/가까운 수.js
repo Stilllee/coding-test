3baadbf58c54afaa708a7c1c48f644fc68424938
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-06-01 21:42:41 +0900

    [level 0] Title: 가까운 수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,14 +1,13 @@
 function solution(array, n) {
-    let closest = array[0];
-    let minDiff = Math.abs(array[0] - n);
-    
-    for (let i = 1; i < array.length; i++) {
-        let diff = Math.abs(array[i] - n);
+    // 배열을 정렬한다. 정렬 기준은 두 가지이다.
+    array.sort((a, b) => {
+        // 1. n과 a, n과 b의 차이의 절대값을 비교해서 차이가 작은 값이 앞에 온다.
+        return Math.abs(n - a) - Math.abs(n - b) ||
         
-        if (diff < minDiff || (diff === minDiff && array[i] < closest)) {
-            minDiff = diff;
-            closest = array[i];
-        }
-    }
-    return closest;
+        // 2. 첫 번째 기준이 같다면, a와 b 자체를 비교해 더 작은 값이 앞에 온다.
+        a - b
+    });
+    
+    // 정렬된 배열의 첫 번째 요소는 n에 가장 가까운 수이거나, 동일한 차이를 가지는 경우 더 작은 수이다.
+    return array[0];
 }
