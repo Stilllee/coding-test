285d84f4e4044d7f0c43fff28b6d3aa26573a0b1
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-06-07 19:54:21 +0900

    [level 0] Title: 로그인 성공?, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -0,0 +1,15 @@
+function solution(id_pw, db) {
+    const [inputId, inputPw] = id_pw;
+    
+    for (const [dbId, dbPw] of db) {
+        if (dbId === inputId) {
+            if (dbPw === inputPw) {
+                return 'login';
+            } else {
+                return 'wrong pw';
+            }
+        }
+    }
+    
+    return 'fail';
+}
