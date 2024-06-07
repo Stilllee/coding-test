fb95fd164fd85440aa573f918cde07a3d2fbac83
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-06-07 16:35:10 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,30 +1,27 @@
 function solution(s) {
-    // 1. 빈 객체 생성
-    let obj = {};
+    // 1. 문자 빈도 카운트
+    const charCount = {};
     
-    // 2. 문자열 순회하면서 객체에 문자: 카운트 증가시키기
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
-        
-        if (obj[char]) {
-            obj[char]++;
+    for (const char of s) {
+        if (charCount[char]) {
+            charCount[char]++;
         } else {
-            obj[char] = 1;
+            charCount[char] = 1;
         }
     }
     
-    // 3. 객체에서 카운트가 1인 문자를 배열에 저장
-    let result = [];
+    // 2. 한 번만 등장하는 문자 필터링
+    const uniqueChars = [];
     
-    for (let char in obj) {
-        if (obj[char] === 1) {
-            result.push(char);
+    for (const char in charCount) {
+        if (charCount[char] === 1) {
+            uniqueChars.push(char);
         }
     }
     
-    // 4. 배열을 사전순으로 정렬
-    result.sort();
+    // 3. 사전 순 정렬
+    uniqueChars.sort();
     
-    // 5. 배열을 문자열로 변환하여 변환, 카운트가 1인 문자가 없다면 빈 문자열 반환
-    return result.join('');
+    // 4. 문자열 반환
+    return uniqueChars.join('');
 }
