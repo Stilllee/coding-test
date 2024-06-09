46a9010a5c419d79745a8ec21c622bd9b855ea43
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-06-10 00:25:48 +0900

    [level 1] Title: 직사각형 별찍기, Time: 43.50 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,22 +1,14 @@
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
+    // 입력 처리
     const n = data.split(" ");
-    const a = Number(n[0]), b = Number(n[1]);
+    const a = Number(n[0])
+    const b = Number(n[1]);
     
-    let result = '';
-    let line = '';
+    // 직사각형 출력
+    const row = '*'.repeat(a);
     
-    for (let i = 1; i <= a; i++) {
-        if (i === a) {
-            line += '*\n'
-        } else {
-            line += '*';   
-        }
+    for (let i = 1; i <= b; i++) {
+        console.log(row);
     }
-    
-    for (let j = 1; j <= b; j++) {
-        result += line;
-    }
-    
-    return console.log(result);
 });
