325d589c0369a22f81919792d04fcbc075296049
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-06-09 22:24:48 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,10 +1,11 @@
 function solution(s) {
-    // 1. 길이 확인
-    const isValidLength = s.length === 4 || s.length === 6;
+	const leng = s.length;
 
-    // 2. 숫자로만 구성되어 있는지 확인
-    const isNumeric = /^\d+$/.test(s);
-
-    // 3. 결과 반환
-    return isValidLength && isNumeric;
+	if (leng !== 4 && leng !== 6) return false;
+	
+	for (let i = 0; i < leng; i++) {
+		if (!('0' <= s[i] && s[i] <= '9')) return false;
+	}
+	
+	return true;
 }
