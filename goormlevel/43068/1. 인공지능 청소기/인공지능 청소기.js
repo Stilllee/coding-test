6100221d55c39c83a232d01e94a103718a3f5e98
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-06-13 19:19:52 +0900

    [goormlevel] Title: 인공지능 청소기, Time: 0, Memory: 0, Difficulty: 1 -BaekjoonHub

--- goormlevel/43068/1. 인공지능 청소기/인공지능 청소기.js
+++ goormlevel/43068/1. 인공지능 청소기/인공지능 청소기.js
@@ -10,8 +10,7 @@ let input = [];
 
 rl.on("line", function(line) {
 	input.push(line);
-	
-	if (input.length -1 == parseInt(input[0])) {
+	if (input.length - 1 == parseInt(input[0])) {
 		rl.close();
 	}
 }).on("close", function() {
@@ -21,7 +20,6 @@ rl.on("line", function(line) {
 	for (let i = 1; i <= T; i++) {
 		const [X, Y, N] = input[i].split(' ').map(Number);
 		
-		// 도달 가능 여부 확인
 		const distance = Math.abs(X) + Math.abs(Y);
 		if (N >= distance && (N - distance) % 2 === 0) {
 			result.push('YES');
@@ -30,7 +28,6 @@ rl.on("line", function(line) {
 		}
 	}
 	
-	// 결과 출력
 	console.log(result.join('\n'));
 	
 	process.exit();
