6ce01cdcb3cd38ece52df55b642f5e43fe3e6ca2
woodstock <108785772+Stilllee@users.noreply.github.com>
2024-06-14 15:30:41 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1556 ms, Memory: 169752 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,11 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const t = parseInt(input[0]);
+let result = '';
+
+for (let i = 1; i <=t; i++) {
+    const [a, b] = input[i].split(' ').map(Number);
+    result += (a + b) + '\n';
+}
+
+console.log(result);
