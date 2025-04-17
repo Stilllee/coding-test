05b4fdfb80e68275b11f8859a1787f2980792c06
beaniedog <orodae@gmail.com>
2025-04-17 15:46:11 +0900

    [level 0] Title: 조건 문자열, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,17 +1,15 @@
-const trueOrFalse = (ineq, eq, n, m) => {
-    const str = ineq + eq;
-    switch(str){
-        case ">=":
-            return n >= m;
-        case "<=":
-            return n <= m;
-        case ">!":
-            return n > m;
-        case "<!":
-            return n < m;
+const evaluateComparison = (ineq, eq, n, m) => {
+    const operator = ineq + eq;
+    
+    switch(operator){
+        case ">=": return n >= m;
+        case "<=": return n <= m;
+        case ">!": return n > m;
+        case "<!": return n < m;
+        default: return false;
     }
 }
 
 function solution(ineq, eq, n, m) {
-    return trueOrFalse(ineq, eq, n, m) ? 1 : 0;
+    return evaluateComparison(ineq, eq, n, m) ? 1 : 0;
 }
