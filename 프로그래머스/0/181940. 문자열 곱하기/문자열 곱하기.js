1788a02e3294ebb784076ffe11c9e416ace4ba11
beaniedog <orodae@gmail.com>
2025-04-17 15:10:26 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,9 +1,3 @@
 function solution(my_string, k) {
-    var answer = '';
-    let i = 0;
-    while (i !== k) {
-        answer += my_string;
-        i++;
-    }
-    return answer;
+    return my_string.repeat(k);
 }
