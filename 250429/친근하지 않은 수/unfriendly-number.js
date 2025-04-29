c3f144c14da150313d8db23843b686914836d4a3
beaniedog <orodae@gmail.com>
2025-04-29 21:20:39 +0900

    Title: 친근하지 않은 수, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

--- 250429/친근하지 않은 수/unfriendly-number.js
+++ 250429/친근하지 않은 수/unfriendly-number.js
@@ -4,8 +4,9 @@ let count = 0;
 
 for (let i = 1 ; i <= n; i++) {
     if (i % 2 === 0 || i % 3 === 0 || i % 5 === 0) {
-        count++;
+        continue;
     }
+    count++;
 }
 
-console.log(n - count);
+console.log(count);
