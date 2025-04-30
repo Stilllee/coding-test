38a700c12e7d32191bb8d8a8ceb94b3823a93b44
beaniedog <orodae@gmail.com>
2025-04-30 14:37:53 +0900

    Title: 별표 출력하기 2, Time: 168ms, Memory: 15MB, Status: Passed - Codetree

--- 250430/별표 출력하기 2/print-star-2.js
+++ 250430/별표 출력하기 2/print-star-2.js
@@ -3,11 +3,10 @@ const n = Number(fs.readFileSync(0).toString().trim());
 
 let result = '';
 
-for (let i = n; i > 0; i--) {
-    for (let j = 0; j < i; j++) {
+for (let i = 0; i < n; i++) {
+    result = '';
+    for (let j = 0; j < n - i; j++) {
         result += '* ';
     }
-    result += '\n';
+    console.log(result);
 }
-
-console.log(result.trim());
