9af3fee2a6e1c1e4f0891f751b9f5c9b7f2e84c3
beaniedog <orodae@gmail.com>
2025-05-03 19:38:14 +0900

    Title: 배열의 합, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

--- 250503/배열의 합/sum-of-array.js
+++ 250503/배열의 합/sum-of-array.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
-for (let line of input) {
-    let total = 0;
-    line = line.split(' ').map(Number);
-    for (let el of line) {
-        total += el;
+// for (let line of input) {
+//     let total = 0;
+//     line = line.split(' ').map(Number);
+//     for (let el of line) {
+//         total += el;
+//     }
+//     console.log(total);
+// }
+
+let arr2d = [];
+for (let i = 0; i < 4; i++) {
+    arr2d.push(input[i].split(' ').map(Number));
+}
+
+for (let i = 0; i < 4; i++) {
+    let sum = 0;
+    for (let j = 0; j < 4; j++) {
+        sum += arr2d[i][j];
     }
-    console.log(total);
+    console.log(sum);
 }
