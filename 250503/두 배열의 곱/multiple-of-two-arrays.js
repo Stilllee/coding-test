6a9eb6202430ed103ae71bab8be704726c7c08ba
beaniedog <orodae@gmail.com>
2025-05-03 19:08:17 +0900

    Title: 두 배열의 곱, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

--- 250503/두 배열의 곱/multiple-of-two-arrays.js
+++ 250503/두 배열의 곱/multiple-of-two-arrays.js
@@ -1,27 +1,28 @@
-const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim().split('\n');
-let arr1 = [];
-let arr2 = [];
-let result = [];
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let arr1 = Array(3).fill(0).map(() => Array(3).fill(0));
+let arr2 = Array(3).fill(0).map(() => Array(3).fill(0));
 
 for (let i = 0; i < 3; i++) {
-    let line = input[i].split(' ').map(Number);
-    arr1.push(line);
-}
-
-for (let i = 4; i < 7; i++) {
-    const line = input[i].split(' ').map(Number);
-    arr2.push(line);
+    arr1[i] = input[i].split(" ").map(Number);
 }
 
 for (let i = 0; i < 3; i++) {
-    let row = [];
+    arr2[i] = input[i + 4].split(" ").map(Number);
+}
+
+let arr3 = Array(3).fill(0).map(() => Array(3).fill(0));
+for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-        row.push(arr1[i][j] * arr2[i][j]);
+        arr3[i][j] = arr1[i][j] * arr2[i][j];
     }
-    result.push(row);
 }
 
-for (let i = 0; i < 3; i++) {
-    console.log(result[i].join(' '));
+for (let row of arr3) {
+    let str = "";
+    for (let elem of row) {
+        str += elem + " ";
+    }
+    console.log(str);
 }
