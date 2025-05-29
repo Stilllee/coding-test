223c434d7f56050b2766dfc70c378637eff241f1
beaniedog <orodae@gmail.com>
2025-05-29 22:48:17 +0900

    Title: 청소당번 정하기, Time: 186ms, Memory: 15MB, Status: Passed - Codetree

--- 250529/청소당번 정하기/cleaning-numbering.js
+++ 250529/청소당번 정하기/cleaning-numbering.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync(0).toString().trim());
 
-let cleaning = [0, 0, 0];
+let cntA = 0, cntB = 0, cntC = 0;
 
-for (let i = 2; i <= n; i++) {
-    if (i % 2 === 0 && i % 3 !== 0 && i % 12 !== 0) {
-        cleaning[0]++;
-    }
-    if (i % 3 === 0 && i % 12 !== 0) {
-        cleaning[1]++;
-    }
-        if (i % 12 === 0) {
-        cleaning[2]++;
+for (let i = 1; i <= n; i++) {
+    if (i % 12 === 0) {
+        cntC++;
+    } else if (i % 3 === 0) {
+        cntB++;
+    } else if (i % 2 === 0) {
+        cntA++;
     }
 }
 
-console.log(cleaning.join(' '));
+console.log(cntA, cntB, cntC);
