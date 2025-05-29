ef41bf81d44a05293d9a7a0637269294c181f2f3
beaniedog <orodae@gmail.com>
2025-05-29 22:36:10 +0900

    Title: 배수의 개수 2, Time: 191ms, Memory: 15MB, Status: Passed - Codetree

--- 250529/배수의 개수 2/number-of-multipliers-2.js
+++ 250529/배수의 개수 2/number-of-multipliers-2.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
 let cnt = 0;
 
 for (let i = 0; i < input.length; i++) {
-    if (i % 2 === 1) {
+    if (input[i] % 2 === 1) {
         cnt++;
     }
 }
