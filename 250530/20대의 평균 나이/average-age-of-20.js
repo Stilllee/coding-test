6f91ef62f0cdf8f141f2352c2c2f0c43c682a07d
beaniedog <orodae@gmail.com>
2025-05-30 16:58:54 +0900

    Title: 20대의 평균 나이, Time: 5035ms, Memory: 18MB, Status: Time Limit Exceed - Codetree

---
+++ 250530/20대의 평균 나이/average-age-of-20.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n').map(Number);
+
+let avr = 0;
+let cnt = 0;
+let idx = 0;
+
+while (true) {
+    const n = input[idx];
+    idx++;
+
+    if (n >= 30) {
+        break;
+    }
+
+    avr += n;
+    cnt++;
+}
+
+console.log((avr / cnt).toFixed(2));
