a0d56bd3f61fa7a6865b528bbd5d907971c6f896
beaniedog <orodae@gmail.com>
2025-05-30 16:05:43 +0900

    Title: 특정 조건의 온전수 구하기, Time: 168ms, Memory: 15MB, Status: Passed - Codetree

--- 250530/특정 조건의 온전수 구하기/print-onjeonsu.js
+++ 250530/특정 조건의 온전수 구하기/print-onjeonsu.js
@@ -4,10 +4,7 @@ const n = Number(fs.readFileSync(0).toString().trim());
 let result = '';
 
 for (let i = 1; i <= n; i++) {
-    const str = i.toString();
-    const lastDigit = str[str.length - 1];
-
-    if (i % 2 === 0 || lastDigit === '5' || (i % 3 === 0 && i % 9 !== 0)) {
+    if (i % 2 === 0 || i % 10 === 5 || (i % 3 === 0 && i % 9 !== 0)) {
         continue;
     }
     result += (i + ' ');
