3eaa5514fcaf49f0f1caf96c4b280866ab2fa22c
beaniedog <orodae@gmail.com>
2025-06-05 16:15:35 +0900

    Title: 별표 출력하기 3, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

--- 250605/별표 출력하기 3/print-star-3.js
+++ 250605/별표 출력하기 3/print-star-3.js
@@ -1,15 +1,19 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync(0).toString());
 
-let str = '';
-
 for (let i = 1; i <= n; i++) {
-    str = '';
-    for (let j = n; j > 2 * n - 1; j--) {
-        str += '* ';
-    }
+    let str = '';
+    
+    // 앞쪽 공백 (i-1개)
     for (let j = 0; j < i - 1; j++) {
-        str += '  ';
+        str += '  ';  // 공백 2칸
     }
+    
+    // 별 개수 = 2*(n에서 i-1만큼 뺀 값) - 1
+    const currentN = n - (i - 1);  // n이 1씩 감소
+    const starCount = 2 * currentN - 1;
+    for (let j = 0; j < starCount; j++) {
+        str += '* ';
+    }   
     console.log(str);
 }
