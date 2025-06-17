3664e095848a69333ae1ec2215c255a091156741
beaniedog <orodae@gmail.com>
2025-06-17 19:19:51 +0900

    Title: 별표 출력하기 8, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250617/별표 출력하기 8/print-star-8.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let str = '';
+
+for (let i = 1; i <= n; i++) {
+    str = '';
+    if (i % 2 === 0) {
+        for (let j = 1; j <= i; j++) {
+            str += '* ';
+        }
+    } else {
+        str += '*';
+    }
+    console.log(str);
+}
