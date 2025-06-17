75a87b8060e31addec0cf32fe25e2ff9041364ea
beaniedog <orodae@gmail.com>
2025-06-17 18:16:26 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 193ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250617/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let str = '';
+
+for (let i = 1; i <= n; i++) {
+    str = '';
+    for (let j = 1; j <= n - i; j++) {
+        str += '  ';
+    }
+    for (let j = 1; j <= i; j++) {
+        str += '@ ';
+    }
+    console.log(str);
+}
+
+for (let i = 1; i < n; i++) {
+    str = '';
+    for (let j = 1; j <= n - i; j++) {
+        str += '@ ';
+    }
+    for (let j = 1; j <= i; j++) {
+        str += '  ';
+    }
+    console.log(str);
+}
