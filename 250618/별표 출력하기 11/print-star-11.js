13aa5e306503d026b4a53c04bbffd1154a5ebc41
beaniedog <orodae@gmail.com>
2025-06-18 19:33:40 +0900

    Title: 별표 출력하기 11, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

--- 250618/별표 출력하기 11/print-star-11.js
+++ 250618/별표 출력하기 11/print-star-11.js
@@ -3,9 +3,9 @@ const n = Number(fs.readFileSync(0).toString().trim());
 
 let str = '';
 
-for (let i = 0; i < (2 ** n) - 1; i++) {
+for (let i = 0; i < 2 * n + 1; i++) {
     str = '';
-    for (let j = 0; j < (2 ** n) - 1; j++) {
+    for (let j = 0; j < 2 * n + 1; j++) {
         if ((i % 2 === 1) && (j % 2 === 1)) {
             str += '  ';
         } else {
