9472f3c54f7fb89fa4ad0ad9b568ebdd25e10210
beaniedog <orodae@gmail.com>
2026-02-09 15:27:53 +0900

    Title: 점수 비교, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260209/점수 비교/score-comparison.py
+++ 260209/점수 비교/score-comparison.py
@@ -1,7 +1,7 @@
 math_a, eng_a = map(int, input().split())
-math_a, eng_b = map(int, input().split())
+math_b, eng_b = map(int, input().split())
 
-if math_a > math_a and eng_a > eng_b:
+if math_a > math_b and eng_a > eng_b:
     print(1)
 else:
     print(0)
