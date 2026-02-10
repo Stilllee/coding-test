ea38446f9e5ea044a44b2df83b71cdca02123ab4
beaniedog <orodae@gmail.com>
2026-02-10 18:04:54 +0900

    Title: 장학금, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260210/장학금/scholarship.py
@@ -0,0 +1,8 @@
+midterm, final = map(int, input().split())
+
+if midterm >= 90 and final >= 95:
+    print(100000)
+elif midterm >= 90 and (final >=90 and final < 95):
+    print(50000)
+else:
+    print(0)
