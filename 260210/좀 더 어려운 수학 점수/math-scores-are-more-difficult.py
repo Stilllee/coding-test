363f953a0aa6c897cc31495c95021b9c336a27ca
beaniedog <orodae@gmail.com>
2026-02-10 18:11:51 +0900

    Title: 좀 더 어려운 수학 점수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260210/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 260210/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,12 +1,7 @@
 math_a, eng_a = map(int, input().split())
 math_b, eng_b = map(int, input().split())
 
-if math_a > math_b:
+if math_a > math_b or (math_a == math_b and eng_a > eng_b):
     print('A')
-elif math_b > math_a:
-    print('B')
 else:
-    if eng_a > eng_b:
-        print('A')
-    else:
-        print('B')
+    print('B')
