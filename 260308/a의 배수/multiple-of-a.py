803d1a0c3fc1e61143ba0c7a1ddddccda91dce60
beaniedog <orodae@gmail.com>
2026-03-08 09:09:45 +0900

    Title: a의 배수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260308/a의 배수/multiple-of-a.py
+++ 260308/a의 배수/multiple-of-a.py
@@ -2,7 +2,7 @@ n, a = map(int, input().split(" "))
 i = 1
 while i <= n:
     if i % a == 0:
-        print(1, end=" ")
+        print(1)
     else:
-        print(0, end=" ")
-i += 1
+        print(0)
+    i += 1
