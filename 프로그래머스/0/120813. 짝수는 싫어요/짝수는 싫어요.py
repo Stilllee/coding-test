31fc5ecec8ad0614cb62452bd6bf3dc5b5b61604
beaniedog <orodae@gmail.com>
2026-03-13 17:01:57 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,8 +1,2 @@
 def solution(n):
-    answer = []
-    
-    for i in range(1, n+1):
-        if i % 2 == 1:
-            answer.append(i)
-    
-    return answer
+    return list(range(1, n+1, 2))
